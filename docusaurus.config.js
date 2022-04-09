21dbe984af76582b41986c028184e1e845b0b8ed
Adam Nativ <admlvntv@gmail.com>
2022-04-09 08:49:56 -0400

    Clean up template
    all work should be done in a pr from now on

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,7 +95,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/team467/',
               },
             ],
           },
